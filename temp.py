7e58c491dba118dd22c6f355fce5305902017dbd
Dimitris Voukatas <dvoukatas@gmail.com>
2025-11-19 18:21:32 +0100

    Data extraction with API

---
+++ temp.py
@@ -0,0 +1,10 @@
+# Create a dictionary with the coordinates of 5 locations:
+locations_dict = {
+    'Copenhagen, DK': {'lat': '55.6761', 'lon': '12.5683'},
+    'Paris, FR': {'lat': '48.85341', 'lon': '2.3488'},
+    'London, GB': {'lat': '51.50853', 'lon': '-0.12574'},
+    'Dubai, AE': {'lat': '25.276987', 'lon': '55.296249'},
+    'Los Angeles, US': {'lat': '34.0522', 'lon': '-118.2437'},
+}
+
+
